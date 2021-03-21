cd5d4cee43a5224216e0979908837898190bacc9
Antonio Caggiano <info@antoniocaggiano.eu>
2021-03-21 23:03:40 +0100

    img: Sampler
    A sampler is needed, together with an image view, to create a combined
    sampler image to be used within a fragment shader.

---
+++ src/sampler.rs
@@ -0,0 +1,49 @@
+// Copyright © 2021
+// Author: Antonio Caggiano <info@antoniocaggiano.eu>
+// SPDX-License-Identifier: MIT;
+
+use std::rc::Rc;
+
+use ash::{version::DeviceV1_0, *};
+
+pub struct Sampler {
+    pub sampler: vk::Sampler,
+    device: Rc<Device>,
+}
+
+impl Sampler {
+    pub fn new(device: &Rc<Device>) -> Self {
+        let device = device.clone();
+
+        let create_info = vk::SamplerCreateInfo::builder()
+            .mag_filter(vk::Filter::NEAREST)
+            .min_filter(vk::Filter::NEAREST)
+            .address_mode_u(vk::SamplerAddressMode::REPEAT)
+            .address_mode_v(vk::SamplerAddressMode::REPEAT)
+            .address_mode_w(vk::SamplerAddressMode::REPEAT)
+            .anisotropy_enable(false)
+            .max_anisotropy(1.0)
+            .border_color(vk::BorderColor::INT_OPAQUE_BLACK)
+            .unnormalized_coordinates(false)
+            .compare_enable(false)
+            .compare_op(vk::CompareOp::ALWAYS)
+            .mipmap_mode(vk::SamplerMipmapMode::LINEAR)
+            .mip_lod_bias(0.0)
+            .min_lod(0.0)
+            .max_lod(0.0)
+            .build();
+
+        let sampler = unsafe { device.create_sampler(&create_info, None) }
+            .expect("Failed to create Vulkan sampler");
+
+        Self { sampler, device }
+    }
+}
+
+impl Drop for Sampler {
+    fn drop(&mut self) {
+        unsafe {
+            self.device.destroy_sampler(self.sampler, None);
+        }
+    }
+}
