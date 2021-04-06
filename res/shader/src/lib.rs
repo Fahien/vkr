751a1bd70b465739b2aeb88393a0be9efa143fd2
Antonio Caggiano <info@antoniocaggiano.eu>
2021-04-06 11:04:22 +0200

    Reinstate Mat block in shaders

--- res/shader/src/lib.rs
+++ res/shader/src/lib.rs
@@ -18,18 +18,23 @@ pub fn line_fs(color: Input<Vec4>, mut out_color: Output<Vec4>) {
     *out_color = *color;
 }
 
+#[spirv(block)]
+pub struct Mat {
+    matrix: Mat4,
+}
+
 #[allow(unused_attributes)]
 #[spirv(vertex)]
 pub fn line_vs(
-    #[spirv(descriptor_set = 0, binding = 0)] model: Uniform<Mat4>,
-    #[spirv(descriptor_set = 1, binding = 0)] view: Uniform<Mat4>,
-    #[spirv(descriptor_set = 1, binding = 1)] proj: Uniform<Mat4>,
+    #[spirv(descriptor_set = 0, binding = 0)] model: Uniform<Mat>,
+    #[spirv(descriptor_set = 1, binding = 0)] view: Uniform<Mat>,
+    #[spirv(descriptor_set = 1, binding = 1)] proj: Uniform<Mat>,
     in_pos: Input<Vec3>,
     in_color: Input<Vec4>,
     mut color: Output<Vec4>,
     #[spirv(position)] mut out_pos: Output<Vec4>,
 ) {
-    *out_pos = *proj * *view * *model * vec4(in_pos.x, in_pos.y, in_pos.z, 1.0);
+    *out_pos = proj.matrix * view.matrix * model.matrix * vec4(in_pos.x, in_pos.y, in_pos.z, 1.0);
     *color = *in_color;
 }
 
@@ -48,9 +53,9 @@ pub fn main_fs(
 #[allow(unused_attributes)]
 #[spirv(vertex)]
 pub fn main_vs(
-    #[spirv(descriptor_set = 0, binding = 0)] model: Uniform<Mat4>,
-    #[spirv(descriptor_set = 1, binding = 0)] view: Uniform<Mat4>,
-    #[spirv(descriptor_set = 1, binding = 1)] proj: Uniform<Mat4>,
+    #[spirv(descriptor_set = 0, binding = 0)] model: Uniform<Mat>,
+    #[spirv(descriptor_set = 1, binding = 0)] view: Uniform<Mat>,
+    #[spirv(descriptor_set = 1, binding = 1)] proj: Uniform<Mat>,
     in_pos: Input<Vec3>,
     in_color: Input<Vec4>,
     in_uv: Input<Vec2>,
@@ -58,7 +63,7 @@ pub fn main_vs(
     mut uv: Output<Vec2>,
     #[spirv(position)] mut out_pos: Output<Vec4>,
 ) {
-    *out_pos = *proj * *view * *model * vec4(in_pos.x, in_pos.y, in_pos.z, 1.0);
+    *out_pos = proj.matrix * view.matrix * model.matrix * vec4(in_pos.x, in_pos.y, in_pos.z, 1.0);
     *color = *in_color;
     uv.x = in_uv.x;
     // UV coords system in Vulkan has inverted Y
