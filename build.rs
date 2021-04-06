62c6d4020beeafb4435472917ae711aa6b67643c
Antonio Caggiano <info@antoniocaggiano.eu>
2021-04-06 15:22:02 +0200

    vkr: Split shaders in two libraries
    The GUI shader needs to be in its own library to prevent Vulkan
    validation errors, therefore we split the shader sub project in two.

--- build.rs
+++ build.rs
@@ -4,10 +4,13 @@
 
 use std::error::Error;
 
-use spirv_builder::{SpirvBuilder, MetadataPrintout};
+use spirv_builder::{MetadataPrintout, SpirvBuilder};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    SpirvBuilder::new("crates/shader", "spirv-unknown-vulkan1.1")
+    SpirvBuilder::new("crates/shader/main", "spirv-unknown-vulkan1.1")
+        .print_metadata(MetadataPrintout::Full)
+        .build()?;
+    SpirvBuilder::new("crates/shader/gui", "spirv-unknown-vulkan1.1")
         .print_metadata(MetadataPrintout::Full)
         .build()?;
     Ok(())
---
+++ crates/shader/gui/src/lib.rs
@@ -0,0 +1,42 @@
+// Copyright © 2021-2022
+// Author: Antonio Caggiano <info@antoniocaggiano.eu>
+// SPDX-License-Identifier: MIT
+
+#![cfg_attr(
+    target_arch = "spirv",
+    feature(register_attr),
+    register_attr(spirv),
+    no_std
+)]
+#![deny(warnings)]
+
+use spirv_std::{
+    glam::{vec4, Mat4, Vec2, Vec4},
+    image::{Image2d, SampledImage},
+};
+
+#[spirv(fragment)]
+pub fn gui_fs(
+    #[spirv(descriptor_set = 0, binding = 0)] image: &SampledImage<Image2d>,
+    uv: Vec2,
+    color: Vec4,
+    out_color: &mut Vec4,
+) {
+    let sample: Vec4 = unsafe { image.sample(uv) };
+    *out_color = color * sample;
+}
+
+#[spirv(vertex)]
+pub fn gui_vs(
+    #[spirv(push_constant)] transform: &Mat4,
+    in_pos: Vec2,
+    in_uv: Vec2,
+    in_color: Vec4,
+    uv: &mut Vec2,
+    color: &mut Vec4,
+    #[spirv(position)] out_pos: &mut Vec4,
+) {
+    *out_pos = *transform * vec4(in_pos.x, in_pos.y, 0.0, 1.0);
+    *uv = in_uv;
+    *color = in_color;
+}
