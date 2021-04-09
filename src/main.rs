57d035e95ce2ae733d3da6be1787b01fa1f40788
Antonio Caggiano <info@antoniocaggiano.eu>
2021-04-09 03:06:52 +0200

    vkr: Move in vkr
    Move most of the graphics objects within the Vkr structure. Change how
    frames behave, but destruction phase needs to be fixed.

--- src/main.rs
+++ src/main.rs
@@ -50,19 +50,9 @@ pub fn main() {
 
     let win = Win::new();
     let (width, height) = win.window.drawable_size();
+    let mut vkr = Vkr::new(win);
 
-    let vkr = Vkr::new(&win);
-
-    let surface = Surface::new(&win, &vkr.ctx);
-    let mut dev = Dev::new(&vkr.ctx, &surface);
-
-    let pass = Pass::new(&mut dev);
-
-    let mut sfs = SwapchainFrames::new(&vkr.ctx, &surface, &mut dev, width, height, &pass);
-
-    let mut gui = Gui::new(&win, &dev, &pass);
-
-    let line_pipeline = Pipeline::line(&dev, &pass, width, height);
+    let line_pipeline = Pipeline::line(&vkr.dev, &vkr.pass, width, height);
 
     let lines_primitive = {
         // Notice how the first line appears at the top of the picture as Vulkan Y axis is pointing downwards
@@ -88,12 +78,12 @@ pub fn main() {
                 Color::new(1.0, 1.0, 0.0, 1.0),
             ),
         ];
-        Primitive::new(&dev.allocator, &lines_vertices)
+        Primitive::new(&vkr.dev.allocator, &lines_vertices)
     };
 
-    let triangle_pipeline = Pipeline::main(&dev, &pass, width, height);
+    let triangle_pipeline = Pipeline::main(&vkr.dev, &vkr.pass, width, height);
 
-    let rect_primitive = Primitive::quad(&dev.allocator);
+    let rect_primitive = Primitive::quad(&vkr.dev.allocator);
 
     let mut model = Model::new();
 
@@ -112,47 +102,23 @@ pub fn main() {
     lines.trs.translate(&na::Vector3::new(0.0, 0.0, -0.5));
     let lines = model.nodes.push(lines);
 
-    let mut events = win.ctx.event_pump().expect("Failed to create SDL events");
+    let image = Image::load(&vkr.dev, "res/image/test.png");
 
-    let image = Image::load(&dev, "res/image/test.png");
-
-    let view = ImageView::new(&dev.device, &image);
+    let view = ImageView::new(&vkr.dev.device, &image);
 
     model.images.push(image);
 
     let view = model.views.push(view);
 
-    let sampler = model.samplers.push(Sampler::new(&dev.device));
+    let sampler = model.samplers.push(Sampler::new(&vkr.dev.device));
 
     let texture = Texture::new(view, sampler);
     let texture = model.textures.push(texture);
 
     'running: loop {
-        let mut resized = false;
-
-        // Handle events
-        for event in events.poll_iter() {
-            match event {
-                Event::Window {
-                    win_event: sdl::event::WindowEvent::Resized(_, _),
-                    ..
-                }
-                | Event::Window {
-                    win_event: sdl::event::WindowEvent::SizeChanged(_, _),
-                    ..
-                } => {
-                    resized = true;
-                }
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
-            }
+        if !vkr.handle_events() {
+            break 'running;
         }
-
-        gui.set_mouse_state(&events.mouse_state());
 
         let delta = timer.get_delta().as_secs_f32();
         let rot = na::UnitQuaternion::from_axis_angle(&na::Vector3::z_axis(), delta / 2.0);
@@ -160,49 +126,13 @@ pub fn main() {
         let rot = na::UnitQuaternion::from_axis_angle(&na::Vector3::z_axis(), -delta / 2.0);
         model.nodes.get_mut(lines).unwrap().trs.rotate(&rot);
 
-        if resized {
-            dev.wait();
-            drop(sfs.swapchain);
-            // Current must be reset to avoid LAYOUT_UNDEFINED validation errors
-            sfs.current = 0;
-            let (width, height) = win.window.drawable_size();
-            sfs.swapchain = Swapchain::new(&vkr.ctx, &surface, &dev, width, height);
-            for i in 0..sfs.swapchain.images.len() {
-                let frame = &mut sfs.frames[i];
-                // Only this semaphore must be recreated to avoid validation errors
-                // The image drawn one is still in use at the moment
-                frame.res.image_ready = Semaphore::new(&dev.device);
-                frame.buffer = Framebuffer::new(&mut dev, &sfs.swapchain.images[i], &pass);
-            }
+        let frame = vkr.begin_frame();
+        if frame.is_none() {
+            continue;
         }
 
-        let frame = sfs.next_frame();
+        let mut frame = frame.unwrap();
 
-        if frame.is_err() {
-            let result = frame.err().unwrap();
-            if result != ash::vk::Result::ERROR_OUT_OF_DATE_KHR {
-                panic!("{:?}", result);
-            }
-
-            dev.wait();
-            drop(sfs.swapchain);
-            let (width, height) = win.window.drawable_size();
-            sfs.swapchain = Swapchain::new(&vkr.ctx, &surface, &dev, width, height);
-            for i in 0..sfs.swapchain.images.len() {
-                let frame = &mut sfs.frames[i];
-                // Only this semaphore must be recreated to avoid validation errors
-                // The image drawn one is still in use at the moment
-                frame.res.image_ready = Semaphore::new(&dev.device);
-                frame.buffer = Framebuffer::new(&mut dev, &sfs.swapchain.images[i], &pass);
-            }
-
-            continue 'running;
-        };
-
-        let frame = frame.unwrap();
-
-        let (width, height) = win.window.drawable_size();
-        frame.begin(&pass, width, height);
         frame.bind(&line_pipeline, &model, camera_node);
         frame.draw::<Line>(
             &line_pipeline,
@@ -214,31 +144,8 @@ pub fn main() {
         frame.bind(&triangle_pipeline, &model, camera_node);
         frame.draw::<Vertex>(&triangle_pipeline, &model, &rect_primitive, rect, texture);
 
-        gui.update(&mut frame.res, delta);
-
-        frame.end();
-
-        match sfs.present(&dev) {
-            // Recreate swapchain
-            Err(ash::vk::Result::ERROR_OUT_OF_DATE_KHR) => {
-                dev.wait();
-                drop(sfs.swapchain);
-                let (width, height) = win.window.drawable_size();
-                sfs.swapchain = Swapchain::new(&vkr.ctx, &surface, &dev, width, height);
-                for i in 0..sfs.swapchain.images.len() {
-                    let frame = &mut sfs.frames[i];
-                    // Semaphores must be recreated to avoid validation errors
-                    frame.res.image_ready = Semaphore::new(&dev.device);
-                    frame.res.image_drawn = Semaphore::new(&dev.device);
-                    frame.buffer = Framebuffer::new(&mut dev, &sfs.swapchain.images[i], &pass);
-                }
-                continue 'running;
-            }
-            Err(result) => panic!("{:?}", result),
-            _ => (),
-        }
+        vkr.end_frame(frame, delta);
     }
 
-    // Make sure device is idle before releasing Vulkan resources
-    dev.wait();
+    vkr.dev.wait();
 }
