3ebd478ba81c174c6ca41f38e212f903ecefde1e
Antonio Caggiano <info@antoniocaggiano.eu>
2022-01-09 18:15:06 +0100

    debug: Warnings and errors
    Move debug in core crate and tweak Vulkan debug callback by printing only
    warning or error messages.

--- crates/vkr-core/src/ctx.rs
+++ crates/vkr-core/src/ctx.rs
@@ -2,9 +2,14 @@
 // Author: Antonio Caggiano <info@antoniocaggiano.eu>
 // SPDX-License-Identifier: MIT
 
-use std::{ffi::CString, marker::PhantomData, os::raw::c_char};
+use std::{
+    borrow::Cow,
+    ffi::{CStr, CString},
+    marker::PhantomData,
+    os::raw::c_char,
+};
 
-use ash::extensions::ext::DebugUtils;
+use ash::{extensions::ext::DebugUtils, vk};
 
 #[cfg(feature = "win")]
 use super::Win;
@@ -59,7 +64,65 @@ impl<'w> CtxBuilder<'w> {
     }
 }
 
+unsafe extern "system" fn vk_debug(
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    _message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _user_data: *mut std::os::raw::c_void,
+) -> vk::Bool32 {
+    if message_severity.intersects(
+        vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
+            | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING,
+    ) {
+        let callback_data = *p_callback_data;
+        let message = if callback_data.p_message.is_null() {
+            Cow::from("No message")
+        } else {
+            CStr::from_ptr(callback_data.p_message).to_string_lossy()
+        };
+        // TODO: M1 complains about this but..
+        if !message.contains("vulkan_memory_model") && !message.contains("VulkanMemoryModel") {
+            eprintln!("{:?}", message);
+        }
+    }
+    ash::vk::FALSE
+}
+
+pub struct Debug {
+    loader: DebugUtils,
+    messenger: vk::DebugUtilsMessengerEXT,
+}
+
+impl Debug {
+    fn new(entry: &ash::Entry, instance: &ash::Instance) -> Self {
+        let loader = DebugUtils::new(entry, instance);
+        let messenger = unsafe {
+            loader
+                .create_debug_utils_messenger(
+                    &vk::DebugUtilsMessengerCreateInfoEXT::builder()
+                        .message_severity(vk::DebugUtilsMessageSeverityFlagsEXT::all())
+                        .message_type(vk::DebugUtilsMessageTypeFlagsEXT::all())
+                        .pfn_user_callback(Some(vk_debug)),
+                    None,
+                )
+                .expect("Failed to create Vulkan debug messenger")
+        };
+
+        Self { loader, messenger }
+    }
+}
+
+impl Drop for Debug {
+    fn drop(&mut self) {
+        unsafe {
+            self.loader
+                .destroy_debug_utils_messenger(self.messenger, None);
+        }
+    }
+}
+
 pub struct Ctx {
+    debug: Option<Debug>,
     pub entry: ash::Entry,
     pub instance: ash::Instance,
 }
@@ -70,7 +133,12 @@ impl Ctx {
     }
 
     pub fn new(extension_names: &[*const c_char]) -> Self {
-        let layers = [CString::new("VK_LAYER_KHRONOS_validation").unwrap()];
+        let is_debug = extension_names.contains(&DebugUtils::name().as_ptr());
+
+        let mut layers = vec![];
+        if is_debug {
+            layers.push(CString::new("VK_LAYER_KHRONOS_validation").unwrap());
+        }
         let layer_names: Vec<*const i8> = layers.iter().map(|name| name.as_ptr()).collect();
 
         let entry = unsafe { ash::Entry::new() }.expect("Failed to create ash entry");
@@ -86,12 +154,24 @@ impl Ctx {
         let instance = unsafe { entry.create_instance(&create_info, None) }
             .expect("Failed to create Vulkan instance");
 
-        Self { entry, instance }
+        let debug = if is_debug {
+            println!("Enabling Vulkan debug utils");
+            Some(Debug::new(&entry, &instance))
+        } else {
+            None
+        };
+
+        Self {
+            debug,
+            entry,
+            instance,
+        }
     }
 }
 
 impl Drop for Ctx {
     fn drop(&mut self) {
+        drop(self.debug.take());
         unsafe {
             self.instance.destroy_instance(None);
         }
